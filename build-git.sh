cc769ede4155a0173f43717156bb7378420f91da
Jeffrey Walton <noloader@gmail.com>
2018-04-04 16:36:06 -0400

    Bump Git version

--- build-git.sh
+++ build-git.sh
@@ -3,8 +3,8 @@
 # Written and placed in public domain by Jeffrey Walton
 # This script builds Git and its dependencies from sources.
 
-GIT_TAR=v2.16.3.tar.gz
-GIT_DIR=git-2.16.3
+GIT_TAR=v2.17.0.tar.gz
+GIT_DIR=git-2.17.0
 
 # Avoid shellcheck.net warning
 CURR_DIR="$PWD"
