a68a1ebb8666d39873f155912bc7994794fbf175
Jeffrey Walton <noloader@gmail.com>
2018-05-19 17:45:49 -0400

    Add workaround to Wget recipe Also see https://lists.gnu.org/archive/html/bug-wget/2018-05/msg00043.html

--- build-wget.sh
+++ build-wget.sh
@@ -149,6 +149,9 @@ cd "$WGET_DIR"
 sed -e 's|sys_lib_dlsearch_path_spec="/lib /usr/lib|sys_lib_dlsearch_path_spec="/lib %{_libdir} /usr/lib|g' configure > configure.fixed
 mv configure.fixed configure; chmod +x configure
 
+sed -e 's|$(LTLIBICONV)|$(LIBICONV)|g' fuzz/Makefile.am > fuzz/Makefile.am.fixed
+mv fuzz/Makefile.am.fixed fuzz/Makefile.am
+
     PKG_CONFIG_PATH="${BUILD_PKGCONFIG[*]}" \
     CPPFLAGS="${BUILD_CPPFLAGS[*]}" \
     CFLAGS="${BUILD_CFLAGS[*]}" \
