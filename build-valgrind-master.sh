936b8ee7e77fa5bdf85d4dae83af51ec3d0cbbec
Jeffrey Walton <noloader@gmail.com>
2018-05-20 22:03:48 -0400

    Add build-valgrind script

---
+++ build-valgrind-master.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds Valgrind from sources.
+
+VALGRIND_DIR=valgrind-master
+PKG_NAME=valgrind
+
+# Avoid shellcheck.net warning
+CURR_DIR="$PWD"
+
+# Sets the number of make jobs if not set in environment
+: "${INSTX_JOBS:=4}"
+
+###############################################################################
+
+# Get the environment as needed. We can't export it because it includes arrays.
+if ! source ./build-environ.sh
+then
+    echo "Failed to set environment"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+if [[ -e "$INSTX_CACHE/$PKG_NAME" ]]; then
+    # Already installed, return success
+    echo ""
+    echo "$PKG_NAME is already installed."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ -z "$SUDO_PASSWORD" ]]; then
+    source ./build-password.sh
+fi
+
+###############################################################################
+
+echo
+echo "********** Valgrind **********"
+echo
+
+rm -rf "$VALGRIND_DIR" 2>/dev/null
+
+git clone git://sourceware.org/git/valgrind.git "$VALGRIND_DIR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to checkout Valgrind"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+cd "$VALGRIND_DIR"
+
+./autogen.sh
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to generate Valgrind build files"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+# http://pkgs.fedoraproject.org/cgit/rpms/gnutls.git/tree/gnutls.spec; thanks NM.
+# AIX needs the execute bit reset on the file.
+sed -e 's|sys_lib_dlsearch_path_spec="/lib /usr/lib|sys_lib_dlsearch_path_spec="/lib %{_libdir} /usr/lib|g' configure > configure.fixed
+mv configure.fixed configure; chmod +x configure
+
+    PKG_CONFIG_PATH="${BUILD_PKGCONFIG[*]}" \
+    CPPFLAGS="-g2 -O3" \
+    CFLAGS="-g2 -O3" \
+    CXXFLAGS="-g2 -O3" \
+    LDFLAGS="" \
+    LIBS="" \
+./configure --prefix="$INSTX_PREFIX" --libdir="$INSTX_LIBDIR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to configure Valgrind"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=("-j" "$INSTX_JOBS")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build Valgrind"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+# Man, Valgirnd is awful when it comes to trying to build self tests.
+# MAKE_FLAGS=("check" "V=1")
+# if ! "$MAKE" "${MAKE_FLAGS[@]}"
+# then
+#    echo "Failed to test Valgrind"
+#    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+# fi
+
+MAKE_FLAGS=("install")
+if [[ ! (-z "$SUDO_PASSWORD") ]]; then
+    echo "$SUDO_PASSWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR"
+
+# Set package status to installed. Delete the file to rebuild the package.
+touch "$INSTX_CACHE/$PKG_NAME"
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$VALGRIND_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-valgrind.sh 2>&1 | tee build-valgrind.log
+    if [[ -e build-valgrind.log ]]; then
+        rm -f build-valgrind.log
+    fi
+fi
+
+[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
