03d39e586b23ff128fd76efbc89340508a25a36e
Jeffrey Walton <noloader@gmail.com>
2018-07-18 08:39:11 -0400

    Add ca_certificate to wgetrc

--- build-wget.sh
+++ build-wget.sh
@@ -185,6 +185,25 @@ if [[ ! "$SKIP_WGET_TESTS" -eq "1" ]]; then
     fi
 fi
 
+# Wget does not have any CA's configured at the moment. HTTPS downloads
+# will fail with the message "... use --no-check-certifcate ...".
+# Fix it through the system's wgetrc configuration file.
+if [[ ! (-z "$SUDO_PASSWORD") ]]; then
+	echo "Copying new-cacert.pem to $SH_CACERT_PATH"
+    echo "$SUDO_PASSWORD" | sudo cp "$HOME/.cacert/cacert.pem" "$SH_CACERT_PATH/new-cacert.pem"
+
+	echo "" >> "./doc/sample.wgetrc"
+	echo "# Default CA zoo file added by Build-Scripts" >> "./doc/sample.wgetrc"
+	echo "ca_certificate = $SH_CACERT_PATH/new-cacert.pem" >> "./doc/sample.wgetrc"
+
+	echo "$SUDO_PASSWORD" | sudo cp "./doc/sample.wgetrc" "$INSTX_PREFIX/etc/wgetrc"
+else
+	echo "" >> "./doc/sample.wgetrc"
+	echo "# Default CA zoo file added by Build-Scripts" >> "./doc/sample.wgetrc"
+	echo "ca_certificate = $HOME/.cacert/cacert.pem" >> "./doc/sample.wgetrc"
+	cp "./doc/sample.wgetrc" "$HOME/.wgetrc"
+fi
+
 MAKE_FLAGS=("install")
 if [[ ! (-z "$SUDO_PASSWORD") ]]; then
     echo "$SUDO_PASSWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
