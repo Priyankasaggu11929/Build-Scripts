1a02f8a725358722ee6dd927090f81f471e9e552
Jeffrey Walton <noloader@gmail.com>
2018-07-18 18:32:09 -0400

    Bump ClamAV version

--- build-clamav.sh
+++ build-clamav.sh
@@ -4,8 +4,8 @@
 # This script builds ClamAV and its dependencies from sources.
 # Also see https://bugzilla.clamav.net/show_bug.cgi?id=11929
 
-CLAMAV_TAR=clamav-0.99.3.tar.gz
-CLAMAV_DIR=clamav-0.99.3
+CLAMAV_TAR=clamav-0.100.1.tar.gz
+CLAMAV_DIR=clamav-0.100.1
 
 # Avoid shellcheck.net warning
 CURR_DIR="$PWD"
