5932c25955353185823698f63debc20e25044d02
Jeffrey Walton <noloader@gmail.com>
2018-07-22 20:26:38 -0400

    Keep configure more recent than other files.

--- fix-config.sh
+++ fix-config.sh
@@ -5,7 +5,7 @@
 
 ###############################################################################
 
-for file in $(find "$PWD" -iname 'configure*')
+for file in $(find "$PWD" -iname 'configure')
 do
 	# Autoconf lib paths are wrong for Fedora and Solaris. Thanks NM.
 	# http://pkgs.fedoraproject.org/cgit/rpms/gnutls.git/tree/gnutls.spec;
@@ -14,6 +14,15 @@ do
 	mv "$file.fixed" "$file"
 	# AIX needs the execute bit reset on the file.
 	chmod +x "$file"
+done
+
+for file in $(find "$PWD" -iname 'configure.ac')
+do
+	# Autoconf lib paths are wrong for Fedora and Solaris. Thanks NM.
+	# http://pkgs.fedoraproject.org/cgit/rpms/gnutls.git/tree/gnutls.spec;
+	sed -e 's|sys_lib_dlsearch_path_spec="/lib /usr/lib|sys_lib_dlsearch_path_spec="/lib %{_libdir} /usr/lib|g' "$file" > "$file.fixed"
+	# Can't use "sed -i" missing on BSDs, OS X and Solaris
+	mv "$file.fixed" "$file"
 	# Keep the filetime old so Autoconf does not re-configure
 	touch -t 197001010000 "$file"
 done
