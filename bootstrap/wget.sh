f45a2fcf95d289a2c038e9a57bd81c67774342e6
Jeffrey Walton <noloader@gmail.com>
2018-12-30 00:03:36 -0500

    Update P11 Kit

--- bootstrap/wget.sh
+++ bootstrap/wget.sh
@@ -76,6 +76,7 @@ if ! patch -u -p0 < wget.patch; then
     exit 1
 fi
 
+    LDFLAGS="-Wl,-Bstatic" \
     PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig/" \
 ./configure \
     --sysconfdir="$PREFIX/etc" \
