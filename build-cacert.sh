7e55ceb4335b7977389cf0e9f6e91e987a6fd367
Jeffrey Walton <noloader@gmail.com>
2018-12-31 10:38:17 -0500

    Add dedicated script for cacerts.pem

---
+++ build-cacert.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script writes several Root CA certifcates needed
+# for other scripts and wget downloads over HTTPS.
+
+PKG_NAME=cacert
+
+###############################################################################
+
+CURR_DIR=$(pwd)
+function finish {
+  cd "$CURR_DIR"
+}
+trap finish EXIT
+
+###############################################################################
+
+# Get the environment as needed. We can't export it because it includes arrays.
+if ! source ./build-environ.sh
+then
+    echo "Failed to set environment"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+if [[ -e "$INSTX_CACHE/$PKG_NAME" ]]; then
+    # Already installed, return success
+    echo ""
+    echo "$PKG_NAME is already installed."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
+fi
+
+# Get a sudo password as needed. The password should die when this
+# subshell goes out of scope.
+if [[ -z "$SUDO_PASSWORD" ]]; then
+    source ./build-password.sh
+fi
+
+###############################################################################
+
+GLOBALSIGN_ROOT="$HOME/.cacert/globalsign-root-r1.pem"
+"$WGET" -q --ca-certificate="$GLOBALSIGN_ROOT" https://curl.haxx.se/ca/cacert.pem -O cacert.pem
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to download cacert.pem"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+if [[ ! (-z "$SUDO_PASSWORD") ]]; then
+    echo "$SUDO_PASSWORD" | sudo -S mkdir -p "$SH_CACERT_PATH"
+    echo "$SUDO_PASSWORD" | sudo -S install -m 644 -t "$SH_CACERT_PATH" cacert.pem
+else
+    mkdir -p "$SH_CACERT_PATH"
+    install -m 644 -t "$SH_CACERT_PATH" cacert.pem
+fi
+
+###############################################################################
+
+# Set package status to installed. Delete the file to rebuild the package.
+touch "$INSTX_CACHE/$PKG_NAME"
+
+[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
