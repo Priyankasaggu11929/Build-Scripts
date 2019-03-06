5fcfbfc28b488ab92982c2769d7b1a7e711feb4c
Jeffrey Walton <noloader@gmail.com>
2019-03-06 08:53:41 -0500

    Bump GnuTLS version

--- build-gnutls.sh
+++ build-gnutls.sh
@@ -3,8 +3,8 @@
 # Written and placed in public domain by Jeffrey Walton
 # This script builds GnuTLS and its dependencies from sources.
 
-GNUTLS_TAR=gnutls-3.6.5.tar.xz
-GNUTLS_DIR=gnutls-3.6.5
+GNUTLS_TAR=gnutls-3.6.6.tar.xz
+GNUTLS_DIR=gnutls-3.6.6
 PKG_NAME=gnutls
 
 ###############################################################################
