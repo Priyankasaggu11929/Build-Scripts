da70c2b3d23869ceb8f0d3bae70664b4d2437bf1
Jeffrey Walton <noloader@gmail.com>
2019-03-07 15:21:30 -0500

    Make libgcrypt UBsan aware

--- build-libgcrypt.sh
+++ build-libgcrypt.sh
@@ -42,6 +42,14 @@ fi
 
 ###############################################################################
 
+if ! ./build-gpgerror.sh
+then
+    echo "Failed to build Libgpg-error"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+###############################################################################
+
 echo
 echo "********** libgcrypt **********"
 echo
@@ -80,8 +88,27 @@ then
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
-MAKE_FLAGS=("check" "V=1")
-if ! "$MAKE" "${MAKE_FLAGS[@]}"
+if [[ "$IS_DARWIN" -ne 0 ]];
+then
+	MAKE_FLAGS=("check" "V=1")
+	if ! DYLD_LIBRARY_PATH="./libs" "$MAKE" "${MAKE_FLAGS[@]}"
+	then
+		echo "Failed to test libgcrypt"
+		[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+	fi
+elif [[ "$IS_LINUX" -ne 0 ]];
+then
+	MAKE_FLAGS=("check" "V=1")
+	if ! LD_LIBRARY_PATH="./libs" "$MAKE" "${MAKE_FLAGS[@]}"
+	then
+		echo "Failed to test libgcrypt"
+		[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+	fi
+fi
+
+echo "Searching for errors hidden in log files"
+COUNT=$(grep -oIR 'runtime error' | wc -l)
+if [[ "${COUNT}" -ne 0 ]];
 then
     echo "Failed to test libgcrypt"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
