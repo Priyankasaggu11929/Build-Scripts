c8b1438e3d73a7143d1440b7e8d1e49f81af0601
Jeffrey Walton <noloader@gmail.com>
2019-03-07 01:57:47 -0500

    Make GMP UBsan aware

--- build-gmp.sh
+++ build-gmp.sh
@@ -74,15 +74,23 @@ if [[ "$?" -ne "0" ]]; then
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
-MAKE_FLAGS=("-j" "$INSTX_JOBS")
+MAKE_FLAGS=("-j" "$INSTX_JOBS" "V=1")
 if ! "$MAKE" "${MAKE_FLAGS[@]}"
 then
     echo "Failed to build GMP"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
-MAKE_FLAGS=("check")
+MAKE_FLAGS=("check" "V=1")
 if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to test GMP"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+echo "Searching for errors hidden in log files"
+COUNT=$(grep -oIR 'runtime error' | wc -l)
+if [[ "${COUNT}" -ne 0 ]];
 then
     echo "Failed to test GMP"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
