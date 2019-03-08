f6cebca49721f9993d25c07574ebcb0fc60fdd92
Jeffrey Walton <noloader@gmail.com>
2019-03-08 01:40:41 -0500

    Cleanup comments in Unistring

--- build-unistr.sh
+++ build-unistr.sh
@@ -93,7 +93,7 @@ fi
 if [[ "$IS_DARWIN" -ne 0 ]];
 then
 	MAKE_FLAGS=("check" "V=1")
-	if ! DYLD_LIBRARY_PATH="./libs" "$MAKE" "${MAKE_FLAGS[@]}"
+	if ! DYLD_LIBRARY_PATH="lib/.libs" "$MAKE" "${MAKE_FLAGS[@]}"
 	then
 		echo "Failed to test Unistring"
 		[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
@@ -101,14 +101,13 @@ then
 elif [[ "$IS_LINUX" -ne 0 ]];
 then
 	MAKE_FLAGS=("check" "V=1")
-	if ! LD_LIBRARY_PATH="./libs" "$MAKE" "${MAKE_FLAGS[@]}"
+	if ! LD_LIBRARY_PATH="lib/.libs" "$MAKE" "${MAKE_FLAGS[@]}"
 	then
 		echo "Failed to test Unistring"
 		[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 	fi
 fi
 
-# Unistring has latent undefined behavior
 echo "Searching for errors hidden in log files"
 COUNT=$(grep -oIR 'runtime error' | wc -l)
 if [[ "${COUNT}" -ne 0 ]];
