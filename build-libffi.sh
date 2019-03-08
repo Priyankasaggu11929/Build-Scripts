37cb7a9801ae99698ab0d64ba49def89fe363a50
Jeffrey Walton <noloader@gmail.com>
2019-03-08 04:42:01 -0500

    Make libffi UBsan aware

--- build-libffi.sh
+++ build-libffi.sh
@@ -92,7 +92,7 @@ echo "Searching for errors hidden in log files"
 COUNT=$(grep -oIR 'runtime error' | wc -l)
 if [[ "${COUNT}" -ne 0 ]];
 then
-    echo "Failed to test libksba"
+    echo "Failed to test libffi"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
