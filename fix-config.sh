602e34b72ece24c4c5b42757d0987718c498e089
Jeffrey Walton <noloader@gmail.com>
2019-05-07 03:56:06 -0400

    Cleanup fix-config.sh

--- fix-config.sh
+++ fix-config.sh
@@ -13,27 +13,22 @@ trap finish EXIT
 
 ###############################################################################
 
+# Autoconf lib paths are wrong for Fedora and Solaris. Thanks NM.
+# http://pkgs.fedoraproject.org/cgit/rpms/gnutls.git/tree/gnutls.spec;
+
 echo "patching sys_lib_dlsearch_path_spec..."
 
 for file in $(find "$PWD" -iname 'configure')
 do
-    # Autoconf lib paths are wrong for Fedora and Solaris. Thanks NM.
-    # http://pkgs.fedoraproject.org/cgit/rpms/gnutls.git/tree/gnutls.spec;
     sed -e 's|sys_lib_dlsearch_path_spec="/lib /usr/lib|sys_lib_dlsearch_path_spec="/lib %{_libdir} /usr/lib|g' "$file" > "$file.fixed"
-    # Can't use "sed -i" missing on BSDs, OS X and Solaris
     chmod +w "$file" && mv "$file.fixed" "$file"
-    # AIX needs the execute bit reset on the file.
     chmod +x "$file"
 done
 
 for file in $(find "$PWD" -iname 'configure.ac')
 do
-    # Autoconf lib paths are wrong for Fedora and Solaris. Thanks NM.
-    # http://pkgs.fedoraproject.org/cgit/rpms/gnutls.git/tree/gnutls.spec;
     sed -e 's|sys_lib_dlsearch_path_spec="/lib /usr/lib|sys_lib_dlsearch_path_spec="/lib %{_libdir} /usr/lib|g' "$file" > "$file.fixed"
-    # Can't use "sed -i" missing on BSDs, OS X and Solaris
     chmod +w "$file" && mv "$file.fixed" "$file"
-    # Keep the filetime old so Autoconf does not re-configure
     touch -t 197001010000 "$file"
 done
 
