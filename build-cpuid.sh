18f403aeb65c608827ff0dc355034ce3c90ab465
Jeffrey Walton <noloader@gmail.com>
2019-05-08 02:40:13 -0400

    Add cpuid recipe

---
+++ build-cpuid.sh
@@ -0,0 +1,125 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds Cpuid from sources.
+
+CPUID_TAR=cpuid-20180519.src.tar.gz
+CPUID_DIR=cpuid-20180519
+PKG_NAME=cpuid
+
+###############################################################################
+
+CURR_DIR=$(pwd)
+function finish {
+  cd "$CURR_DIR"
+}
+trap finish EXIT
+
+# Sets the number of make jobs if not set in environment
+: "${INSTX_JOBS:=4}"
+
+###############################################################################
+
+# Get the environment as needed. We can't export it because it includes arrays.
+if ! source ./setup-environ.sh
+then
+    echo "Failed to set environment"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ -z "$SUDO_PASSWORD" ]]; then
+    source ./setup-password.sh
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+###############################################################################
+
+echo
+echo "********** Cpuid **********"
+echo
+
+if ! "$WGET" -O "$CPUID_TAR" --ca-certificate="$LETS_ENCRYPT_ROOT" \
+     "http://www.etallen.com/cpuid/$CPUID_TAR"
+then
+    echo "Failed to download Cpuid"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+rm -rf "$CPUID_DIR" &>/dev/null
+gzip -d < "$CPUID_TAR" | tar xf -
+cd "$CPUID_DIR"
+
+cp ../patch/cpuid.patch .
+patch -u -p0 < cpuid.patch
+echo ""
+
+MAKE_FLAGS=("-j" "$INSTX_JOBS")
+if ! PKG_CONFIG_PATH="${BUILD_PKGCONFIG[*]}" \
+     CPPFLAGS="${BUILD_CPPFLAGS[*]}" \
+     CFLAGS="${BUILD_CFLAGS[*]}" \
+     CXXFLAGS="${BUILD_CXXFLAGS[*]}" \
+     LDFLAGS="${BUILD_LDFLAGS[*]}" \
+     LIBS="${BUILD_LIBS[*]}" \
+    "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build Cpuid"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+# No make check program
+#MAKE_FLAGS=("check")
+#if ! "$MAKE" "${MAKE_FLAGS[@]}"
+#then
+#    echo "Failed to test Cpuid"
+#    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+#fi
+
+echo "Searching for errors hidden in log files"
+COUNT=$(grep -oIR 'runtime error:' ./* | wc -l)
+if [[ "${COUNT}" -ne 0 ]];
+then
+    echo "Failed to test Cpuid"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=("install")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    echo "$SUDO_PASSWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR"
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Cpuid's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if false; then
+
+    ARTIFACTS=("$CPUID_TAR" "$CPUID_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-cpuid.sh 2>&1 | tee build-cpuid.log
+    if [[ -e build-cpuid.log ]]; then
+        rm -f build-cpuid.log
+    fi
+fi
+
+[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
