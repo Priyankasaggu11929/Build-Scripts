f32129fc3a0a466bb591a7949ebc003ac2d2aee2
Jeffrey Walton <noloader@gmail.com>
2019-05-13 17:29:10 -0400

    Add progress messages

--- build-cmake.sh
+++ build-cmake.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 # Written and placed in public domain by Jeffrey Walton
-# This script builds ClamAV and its dependencies from sources.
-# Also see https://bugzilla.clamav.net/show_bug.cgi?id=11929
+# This script builds CMake and its dependencies from sources.
 
-CLAMAV_TAR=clamav-0.101.2.tar.gz
-CLAMAV_DIR=clamav-0.101.2
+CMAKE_TAR=cmake-3.14.3.tar.gz
+CMAKE_DIR=cmake-3.14.3
 
 ###############################################################################
 
@@ -21,20 +20,21 @@ trap finish EXIT
 ###############################################################################
 
 # Get the environment as needed. We can't export it because it includes arrays.
-if ! source ./setup-environ.sh
+if ! source ./build-environ.sh
 then
     echo "Failed to set environment"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
-# The password should die when this subshell goes out of scope
+# Get a sudo password as needed. The password should die when this
+# subshell goes out of scope.
 if [[ -z "$SUDO_PASSWORD" ]]; then
-    source ./setup-password.sh
+    source ./build-password.sh
 fi
 
 ###############################################################################
 
-if ! ./build-cacert.sh
+if ! ./build-cacerts.sh
 then
     echo "Failed to install CA Certs"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
@@ -42,67 +42,30 @@ fi
 
 ###############################################################################
 
-if ! ./build-zlib.sh
-then
-    echo "Failed to build zLib"
-    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
-fi
-
-###############################################################################
-
-if ! ./build-bzip.sh
-then
-    echo "Failed to build Bzip2"
-    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
-fi
-
-###############################################################################
-
-if ! ./build-openssl.sh
-then
-    echo "Failed to build OpenSSL"
-    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
-fi
-
-###############################################################################
-
-if ! ./build-pcre.sh
-then
-    echo "Failed to build PCRE and PCRE2"
-    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
-fi
-
-###############################################################################
-
 echo
-echo "********** ClamAV **********"
+echo "********** CMake **********"
 echo
 
-if ! -O "$CLAMAV_TAR" "$WGET" --ca-certificate="$CA_ZOO" \
-     "https://www.clamav.net/downloads/production/$CLAMAV_TAR"
+if ! "$WGET" -O "$CMAKE_TAR" --ca-certificate="$DIGICERT_ROOT" \
+     "https://github.com/Kitware/CMake/releases/download/v3.14.3/$CMAKE_TAR"
 then
-    echo "Failed to download ClamAV"
+    echo "Failed to download CMake"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
-rm -rf "$CLAMAV_DIR" &>/dev/null
-gzip -d < "$CLAMAV_TAR" | tar xf -
-cd "$CLAMAV_DIR"
+rm -rf "$CMAKE_DIR" &>/dev/null
+gzip -d < "$CMAKE_TAR" | tar xf -
 
-# Fix sys_lib_dlsearch_path_spec and keep the file time in the past
-../fix-config.sh
+cd "$CMAKE_DIR"
 
-    PKG_CONFIG_PATH="${BUILD_PKGCONFIG[*]}" \
-    CPPFLAGS="${BUILD_CPPFLAGS[*]}" \
-    CFLAGS="${BUILD_CFLAGS[*]}" \
-    CXXFLAGS="${BUILD_CXXFLAGS[*]}" \
-    LDFLAGS="${BUILD_LDFLAGS[*]}" \
-    LIBS="${BUILD_LIBS[*]}" \
-./configure --prefix="$INSTX_PREFIX" --libdir="$INSTX_LIBDIR" \
-    --with-openssl-dir="$INSTX_PREFIX" --with-zlib="$INSTX_PREFIX"
+echo "**********************"
+echo "Bootstrapping package"
+echo "**********************"
 
-if [[ "$?" -ne 0 ]]; then
-    echo "Failed to configure ClamAV"
+# This is the CMake build command per https://cmake.org/install/
+if ! ./bootstrap
+then
+    echo "Failed to bootstrap CMake"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
@@ -113,7 +76,7 @@ echo "**********************"
 MAKE_FLAGS=("-j" "$INSTX_JOBS")
 if ! "$MAKE" "${MAKE_FLAGS[@]}"
 then
-    echo "Failed to build ClamAV"
+    echo "Failed to build CMake"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
@@ -121,18 +84,18 @@ echo "**********************"
 echo "Testing package"
 echo "**********************"
 
-MAKE_FLAGS=("check")
+MAKE_FLAGS=("check" "V=1")
 if ! "$MAKE" "${MAKE_FLAGS[@]}"
 then
-    echo "Failed to test ClamAV"
-    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+   echo "Failed to test CMake"
+   [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
 echo "Searching for errors hidden in log files"
 COUNT=$(grep -oIR 'runtime error:' ./* | wc -l)
 if [[ "${COUNT}" -ne 0 ]];
 then
-    echo "Failed to test ClamAV"
+    echo "Failed to test CMake"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
@@ -141,7 +104,7 @@ echo "Installing package"
 echo "**********************"
 
 MAKE_FLAGS=("install")
-if [[ -n "$SUDO_PASSWORD" ]]; then
+if [[ ! (-z "$SUDO_PASSWORD") ]]; then
     echo "$SUDO_PASSWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
 else
     "$MAKE" "${MAKE_FLAGS[@]}"
@@ -161,14 +124,14 @@ echo "**************************************************************************
 # Set to false to retain artifacts
 if true; then
 
-    ARTIFACTS=("$CLAMAV_TAR" "$CLAMAV_DIR")
+    ARTIFACTS=("$CMAKE_TAR" "$CMAKE_DIR")
     for artifact in "${ARTIFACTS[@]}"; do
         rm -rf "$artifact"
     done
 
-    # ./build-clamav.sh 2>&1 | tee build-clamav.log
-    if [[ -e build-clamav.log ]]; then
-        rm -f build-clamav.log
+    # ./build-cmake.sh 2>&1 | tee build-cmake.log
+    if [[ -e build-cmake.log ]]; then
+        rm -f build-cmake.log
     fi
 
     unset SUDO_PASSWORD
