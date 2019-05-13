172639be5552fde3d3960ac14a7be004bd973440
Jeffrey Walton <noloader@gmail.com>
2019-05-13 17:24:08 -0400

    Add progress messages

--- build-emacs.sh
+++ build-emacs.sh
@@ -113,12 +113,20 @@ if [[ "$?" -ne 0 ]]; then
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
+echo "**********************"
+echo "Building package"
+echo "**********************"
+
 MAKE_FLAGS=("V=1")
 if ! "$MAKE" "${MAKE_FLAGS[@]}"
 then
    echo "Failed to test Emacs"
    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
+
+#echo "**********************"
+#echo "Testing package"
+#echo "**********************"
 
 #MAKE_FLAGS=("check" "V=1")
 #if ! "$MAKE" "${MAKE_FLAGS[@]}"
@@ -135,12 +143,9 @@ fi
 #    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 #fi
 
-MAKE_FLAGS=("-j" "$INSTX_JOBS" "all")
-if ! "$MAKE" "${MAKE_FLAGS[@]}"
-then
-    echo "Failed to build emacs"
-    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
-fi
+echo "**********************"
+echo "Installing package"
+echo "**********************"
 
 MAKE_FLAGS=("install")
 if [[ -n "$SUDO_PASSWORD" ]]; then
