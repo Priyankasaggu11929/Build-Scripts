31b80e2274aa08991b5a48e4efbc81dd19bb5d3c
Jeffrey Walton <noloader@gmail.com>
2019-05-13 20:09:30 -0400

    Cleanup LDNS recipe

--- build-ldns.sh
+++ build-ldns.sh
@@ -126,12 +126,12 @@ echo "Testing package"
 echo "**********************"
 
 # 'make test' fails for 1.7.0
-#MAKE_FLAGS=("test")
-#if ! "$MAKE" "${MAKE_FLAGS[@]}"
-#then
-#    echo "Failed to test LDNS"
-#    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
-#fi
+MAKE_FLAGS=("test")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to test LDNS"
+    # [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
 
 echo "Searching for errors hidden in log files"
 COUNT=$(grep -oIR 'runtime error:' ./* | wc -l)
