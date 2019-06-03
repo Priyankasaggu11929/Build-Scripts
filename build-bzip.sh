af2814f6beb95c5fa0fdbf1410b2e7e2ee5dadeb
Jeffrey Walton <noloader@gmail.com>
2019-06-03 04:03:38 -0400

    Bump Bzip2 to 1.0.6-3

--- build-bzip.sh
+++ build-bzip.sh
@@ -6,8 +6,8 @@
 # Bzip lost its website. This build of Bzip is based on the last known
 # release bzip-1.0.6. Also see https://github.com/noloader/bzip2-noloader.
 
-BZIP2_TAR=BZIP2_1_0_6_2.tar.gz
-BZIP2_DIR=BZIP2_1_0_6_2
+BZIP2_TAR=BZIP2_1_0_6_3.tar.gz
+BZIP2_DIR=BZIP2_1_0_6_3
 PKG_NAME=bzip2
 
 ###############################################################################
