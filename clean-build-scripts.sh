a0d75922ad12d3bd78ffaadf0a11c1e889ceb65c
Jeffrey Walton <noloader@gmail.com>
2019-06-07 12:33:19 -0400

    Cleanup clean recipe

---
+++ clean-build-scripts.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script cleans artifacts created by the Build Scripts
+# If you installed something in a non-standard location, then
+# you will need to delete it manually.
+
+# Run the script like so:
+#
+#    sudo ./clean-build-scripts.sh
+
+rm -rf ~/.cacerts
+rm -rf ~/.build-scripts
+rm -rf ~/bootstrap
+rm -rf ~/usr/local
