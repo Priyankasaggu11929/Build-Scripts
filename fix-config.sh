b2b33aedff5008b4260d7b2c2a3230fdb247eb03
Jeffrey Walton <noloader@gmail.com>
2019-07-19 13:08:46 -0400

    Fix fix-config.sh and configure timestamp

--- fix-config.sh
+++ fix-config.sh
@@ -18,15 +18,17 @@ trap finish EXIT
 
 echo "patching sys_lib_dlsearch_path_spec..."
 
+# Keep configure in the future
 for file in $(find "$PWD" -iname 'configure')
 do
     cp -p "$file" "$file.fixed"
     chmod +w "$file" && chmod +w "$file.fixed"
     sed -e 's|sys_lib_dlsearch_path_spec="/lib /usr/lib|sys_lib_dlsearch_path_spec="/lib %{_libdir} /usr/lib|g' "$file" > "$file.fixed"
     mv "$file.fixed" "$file" && chmod +x "$file"
-    touch -t 197001010000 "$file"
+    touch "$file"
 done
 
+# And keep configure in the past
 for file in $(find "$PWD" -iname 'configure.ac')
 do
     cp -p "$file" "$file.fixed"
@@ -38,16 +40,20 @@ done
 
 echo "patching config.sub..."
 if [[ -e ../../patch/config.sub ]]; then
-    find "$PWD" -name config.sub -exec bash -c 'cp ../../patch/config.sub "$1"' _ {} \;
+    find "$PWD" -name config.sub \
+		-exec bash -c 'chmod +w "$1" && cp ../../patch/config.sub "$1"' _ {} \;
 else
-    find "$PWD" -name config.sub -exec bash -c 'cp ../patch/config.sub "$1"' _ {} \;
+    find "$PWD" -name config.sub \
+		-exec bash -c 'chmod +w "$1" && cp ../patch/config.sub "$1"' _ {} \;
 fi
 
 echo "patching config.guess..."
 if [[ -e ../../patch/config.guess ]]; then
-    find "$PWD" -name config.guess -exec bash -c 'cp ../../patch/config.guess "$1"' _ {} \;
+    find "$PWD" -name config.guess \
+		-exec bash -c 'chmod +w "$1" && cp ../../patch/config.guess "$1"' _ {} \;
 else
-    find "$PWD" -name config.guess -exec bash -c 'cp ../patch/config.guess "$1"' _ {} \;
+    find "$PWD" -name config.guess \
+		-exec bash -c 'chmod +w "$1" && cp ../patch/config.guess "$1"' _ {} \;
 fi
 
 echo ""
